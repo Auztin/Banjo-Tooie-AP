a9db4f2d0d2362feef6d5b1fbf3ce6dee192e0e2
jjjj12212 <mjackson12212@hotmail.com>
2024-11-07 20:08:57 -0700

    GetSlotData. Encouragement Table needs to be shuffled.

--- pc/src/client/bt_client.cpp
+++ pc/src/client/bt_client.cpp
@@ -69,8 +69,213 @@ asio::awaitable<void> BTClient::getSlotData()
     }
     string msg = retTable.dump() + "\n";
     co_await send(msg);
-    json j = co_await read();
+    json block = co_await read();
+    if(block.contains(string{"slot_player"}) && block["slot_player"] != "")
+    {
+        PLAYER = block["slot_player"];
+        if(DEBUG_NET == true) { std::cout << "Player is: " << PLAYER << std::endl; }
+    }
+    if(block.contains(string{"slot_seed"}) && block["slot_seed"] != "")
+    {
+        SEED = block["slot_seed"];
+        if(DEBUG_NET == true) { std::cout << "SEED is: " << SEED << std::endl; }
+    }
+    if(block.contains(string{"slot_deathlink"}) && block["slot_deathlink"] != "false")
+    {
+        DEATH_LINK = true;
+        if(DEBUG_NET == true) { std::cout << "Deathlink is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_skip_tot"}) && block["slot_skip_tot"] != "false")
+    {
+        SKIP_TOT = true;
+        if(DEBUG_NET == true) { std::cout << "SKIP TOT is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_bkmoves"}) && block["slot_bkmoves"] != "false")
+    {
+        ENABLE_AP_BK_MOVES = true;
+        if(DEBUG_NET == true) { std::cout << "BK_MOVES is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_cheatorewards"}) && block["slot_cheatorewards"] != "false")
+    {
+        ENABLE_AP_CHEATO_REWARDS = true;
+        if(DEBUG_NET == true) { std::cout << "CHEATO_REWARDS is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_honeybrewards"}) && block["slot_honeybrewards"] != "false")
+    {
+        ENABLE_AP_HONEYB_REWARDS = true;
+        if(DEBUG_NET == true) { std::cout << "HONEYB_REWARDS is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_minigames"}) && block["slot_minigames"] != "")
+    {
+        MINIGAMES = block["slot_minigames"];
+        if(DEBUG_NET == true) { std::cout << "MINIGAMES set to " << MINIGAMES << std::endl; }
+    }
+    if(block.contains(string{"slot_skip_puzzles"}) && block["slot_skip_puzzles"] != "false")
+    {
+        SKIP_PUZZLES = true;
+        if(DEBUG_NET == true) { std::cout << "SKIP_PUZZLES is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_skip_klungo"}) && block["slot_skip_klungo"] != "false")
+    {
+        SKIP_KLUNGO = true;
+        if(DEBUG_NET == true) { std::cout << "SKIP_KLUNGO is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_open_hag1"}) && block["slot_open_hag1"] != "false")
+    {
+        OPEN_HAG1 = true;
+        if(DEBUG_NET == true) { std::cout << "OPEN_HAG1 is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_chuffy"}) && block["slot_chuffy"] != "false")
+    {
+        ENABLE_AP_CHUFFY = true;
+        if(DEBUG_NET == true) { std::cout << "CHUFFY is set" << std::endl; }
+    }
+    if(block.contains(string{"slot_worlds"}) && block["slot_worlds"] != "false")
+    {
+        ENABLE_AP_WORLDS = true;
+        if(DEBUG_NET == true) { std::cout << "Worlds are Shuffled" << std::endl; }
+    }
+    if(block.contains(string{"slot_mystery"}) && block["slot_mystery"] != "false")
+    {
+        ENABLE_AP_MYSTERY = true;
+        if(DEBUG_NET == true) { std::cout << "StopNSwap is Randomized" << std::endl; }
+    }
+    if(block.contains(string{"slot_goal_type"}) && block["slot_goal_type"] != "")
+    {
+        GOAL_TYPE = block["slot_goal_type"];
+        if(DEBUG_NET == true) { std::cout << "GOAL_TYPE is set to " << GOAL_TYPE << std::endl; }
+    }
+    if(block.contains(string{"slot_minigame_hunt_length"}) && block["slot_minigame_hunt_length"] != "")
+    {
+        MGH_LENGTH = block["slot_minigame_hunt_length"];
+    }
+    if(block.contains(string{"slot_boss_hunt_length"}) && block["slot_boss_hunt_length"] != "")
+    {
+        BH_LENGTH = block["slot_boss_hunt_length"];
+    }
+    if(block.contains(string{"slot_jinjo_family_rescue_length"}) && block["slot_jinjo_family_rescue_length"] != "")
+    {
+        JFR_LENGTH = block["slot_jinjo_family_rescue_length"];
+    }
+    if(block.contains(string{"slot_token_hunt_length"}) && block["slot_token_hunt_length"] != "")
+    {
+        TH_LENGTH = block["slot_token_hunt_length"];
+    }
+    if(block.contains(string{"slot_world_order"}))
+    {
+        // Here is you set Which Level to open in which order and set Jiggy Amount for each world.
+        //slot_world_order is a Table.. Iterate in order using slot_keys. Below is the lua example:
+        /*
+            for level, jiggy_amt in pairs(block['slot_world_order'])
+            do
+                local locationId = block['slot_keys'][level]
+                if level == "Outside Grunty's Industries"
+                then
+                    level = "Grunty Industries"
+                elseif  level == "Jolly Roger's Lagoon - Town Center"
+                then
+                    level = "Jolly Roger's Lagoon"
+                end
+                for worlds, t in pairs(WORLD_ENTRANCE_MAP)
+                do
+                    if t['defaultName'] == level
+                    then
+                        WORLD_ENTRANCE_MAP[worlds]["defaultCost"] = jiggy_amt
+                        WORLD_ENTRANCE_MAP[worlds]["locationId"] = tostring(locationId)
+                    end
+                end
+            end
+        */
+    }
+    if(block.contains(string{"slot_open_silo"}))
+    {
+        OPEN_SILO = block["slot_open_silo"];
+    }
+    if(block.contains(string{"slot_version"}) && block["slot_version"] != "")
+    {
+        CLIENT_VERSION = block["slot_version"];
+        if(CLIENT_VERSION != CPP_VERSION)
+        {
+            VERSION_ERR = true;
+            co_return;
+        }
+    }
+    if(block.contains(string{"slot_zones"}))
+    {
+        //This Contains a table of Which Loading Zones enter to Which Worlds.
+        //This will later Exand to more maps, but for now, its just the Front Entrances
+    }
+    if(SEED != 0){
+        printGoalInfo();
+    }  
     co_return;
+}
+
+void BTClient::printGoalInfo()
+{
+    const std::vector<string> encouragement{
+        " GUH-HUH!",
+        " BREEE!",
+        " EEKUM BOKUM!",
+        " YEEHAW!",
+        " JINJOO!!",
+        " WAHEY!!!",
+        " ROOOOO!!!",
+        " OOMANAKA!!!"
+    };
+    // auto rng = std::default_random_engine {};
+    // std::shuffle(std::begin(encouragement), std::end(encouragement), rng);
+    // Shuffle the Encouragement and pick one.
+    if(GOAL_TYPE == 0)
+    {
+        std::cout << "You need to hunt down Grunty in her HAG1" << std::endl;
+        std::cout << "and put her back in the ground!" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 1 && MGH_LENGTH == 15)
+    {
+        std::cout << "You are hunting down all 15 of the Mumbo Tokens" << std::endl;
+        std::cout << "found in Grunty's dastardly minigames! Good luck and" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 1 && MGH_LENGTH < 15)
+    {
+        std::cout << "You are hunting for " << MGH_LENGTH << " Mumbo Tokens from" << std::endl;
+        std::cout << "Grunty's dastardly minigames! Good luck and" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 2 && BH_LENGTH == 8)
+    {
+        std::cout << "You are hunting down all 8 Mumbo Tokens from" << std::endl;
+        std::cout << "each world boss! Good Luck and" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 2 && BH_LENGTH < 8)
+    {
+        std::cout << "You are hunting for " << BH_LENGTH << " Mumbo Tokens from" << std::endl;
+        std::cout << "the 8 world bosses! Good Luck and" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 3 && JFR_LENGTH == 9)
+    {
+        std::cout << "You are trying to rescue all 9 Jinjo families and" << std::endl;
+        std::cout << "retrieve their Mumbo Tokens! Good Luck and" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 3 && JFR_LENGTH < 9)
+    {
+        std::cout << "You are trying to rescue " << JFR_LENGTH << " of the 9 Jinjo families" << std::endl;
+        std::cout << "and retrieve their Mumbo Tokens! Good Luck and" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 4)
+    {
+        std::cout << "You absolute mad lad! You're doing the Wonder Wing Challenge! Good Luck and" << std::endl;
+    }
+    else if(GOAL_TYPE == 5 && TH_LENGTH == 15)
+    {
+        std::cout << "You are trying to find all 15 of Mumbo's Tokens scattered" << std::endl;
+        std::cout << "throughout the Isle of Hags! Good Luck and" << encouragement[0] << std::endl;
+    }
+    else if(GOAL_TYPE == 5 && TH_LENGTH < 15)
+    {
+        std::cout << "You are trying to find " << TH_LENGTH << " of the 15 of Mumbo Tokens" << std::endl;
+        std::cout << "scattered throughout the Isle of Hags! Good Luck and" << encouragement[0] << std::endl;
+    }
+    return;
 }
 
 asio::awaitable<void> BTClient::receive()
