278fa1a9946285a249158ee3bbdcda1a578e518f
jjjj12212 <mjackson12212@hotmail.com>
2024-11-07 20:08:57 -0700

    GetSlotData. Encouragement Table needs to be shuffled.

--- pc/src/client/bt_client.hpp
+++ pc/src/client/bt_client.hpp
@@ -4,6 +4,10 @@
 #include <asio.hpp>
 #include <asio/awaitable.hpp>
 #include <json.hpp>
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <vector>
 
 class BTClient {
 public:
@@ -21,7 +25,7 @@ private:
   bool check_state();
   asio::awaitable<void> every_30frames();
   // // void every_5frames();
-
+  const std::string CPP_VERSION = "V3.3.3"; //Make sure this matches with the BT_Client Version
   const std::string STATE_OK = "Ok";
   const std::string STATE_TENTATIVELY_CONNECTED = "Tentatively Connected";
   const std::string STATE_INITIAL_CONNECTION_MADE = "Initial Connection Made";
@@ -30,12 +34,34 @@ private:
   std::string CUR_STATE = STATE_UNINITIALIZED;
   std::string PREV_STATE = "";
   std::string PLAYER = "";
+  bool DEATH_LINK = false;
+  bool SKIP_TOT = false;
+  bool ENABLE_AP_BK_MOVES = false;
+  bool ENABLE_AP_CHEATO_REWARDS = false;
+  bool ENABLE_AP_HONEYB_REWARDS = false;
+  std::string MINIGAMES = "";
+  bool SKIP_PUZZLES = false;
+  bool SKIP_KLUNGO = false;
+  bool OPEN_HAG1 = false;
+  bool ENABLE_AP_CHUFFY = false;
+  bool ENABLE_AP_WORLDS = false; //Used to Randomize World Order
+  bool ENABLE_AP_MYSTERY = false; //StopNSwap
+  int GOAL_TYPE = 0;
+  int MGH_LENGTH = 0; //Mini-game Mumbo Token Length
+  int BH_LENGTH = 0; //Boss Token Length
+  int JFR_LENGTH = 0; //Jinjo Token Length
+  int TH_LENGTH = 0; //Token Hunt Length
+  std::string OPEN_SILO = "NONE"; //Which Silo is Open
+  std::string CLIENT_VERSION = "V0.0";
+  bool VERSION_ERR = false;
+
   int SEED = 0;
   asio::awaitable<void> receive();
   asio::awaitable<void> getSlotData();
   bool DEBUG_NET = false;
   asio::awaitable<void> send(std::string jsonData);
   asio::awaitable<nlohmann::json> read();
+  void printGoalInfo();
 };
 
 #endif // BT_CLIENT_HPP
