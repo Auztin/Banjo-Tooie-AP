e21314fd59481b85eee2772ad3bcbc1d93eefffe
Austin <6369485+Auztin@users.noreply.github.com>
2024-11-10 15:52:14 -0600

    use typedef instead

--- include/ap_memory.h
+++ include/ap_memory.h
@@ -3,28 +3,28 @@
 
 #include <stdint.h>
 
-#define u8 uint8_t
-#define u16 uint16_t
-#define u32 uint32_t
-#define u64 uint64_t
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
-#define s8 int8_t
-#define s16 int16_t
-#define s32 int32_t
-#define s64 int64_t
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
 
-#define vu8 volatile uint8_t
-#define vu16 volatile uint16_t
-#define vu32 volatile uint32_t
-#define vu64 volatile uint64_t
+typedef volatile uint8_t vu8;
+typedef volatile uint16_t vu16;
+typedef volatile uint32_t vu32;
+typedef volatile uint64_t vu64;
 
-#define vs8 volatile int8_t
-#define vs16 volatile int16_t
-#define vs32 volatile int32_t
-#define vs64 volatile int64_t
+typedef volatile int8_t vs8;
+typedef volatile int16_t vs16;
+typedef volatile int32_t vs32;
+typedef volatile int64_t vs64;
 
-#define f32 float
-#define f64 double
+typedef float f32;
+typedef double f64;
 
 #include "ap_memory/n64.h"
 #include "ap_memory/pc.h"
