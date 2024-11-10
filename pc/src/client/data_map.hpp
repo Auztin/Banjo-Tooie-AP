222e5a3a9b97044de4b45f7690a30ba2aeca419c
jjjj12212 <mjackson12212@hotmail.com>
2024-11-09 21:26:31 -0700

    Need more stuff

---
+++ pc/src/client/data_map.hpp
@@ -0,0 +1,14 @@
+#ifndef BT_MAP_HPP
+    #define BT_MAP_HPP
+
+    #include <iostream>
+    #include <map>
+    #include <string>
+    #include <vector>
+    #include <ap_memory.h>
+    #include <bt/save_flags.h>
+
+    extern std::map<uint16_t, std::map<std::string, std::vector<std::string>>> ASSET_MAP_CHECK;
+    // const std::map<std::string, std::string> ADDRESS_MAP;
+    bool check_flag(std::string);
+#endif
