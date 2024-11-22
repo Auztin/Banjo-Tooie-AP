48f614320661b8125817e780cd19c9f50392b576
Austin <6369485+Auztin@users.noreply.github.com>
2024-11-22 10:59:55 -0600

    move refill air

--- c/bt/player_chars.h
+++ c/bt/player_chars.h
@@ -18,9 +18,14 @@ typedef struct {
 typedef u32 (*bt_fnt_hurt_player)(u32, u32, u32);
 #define _bt_fn_hurt_player ((bt_fnt_hurt_player)0x800F79DC)
 #define bt_fn_hurt_player(control_index) _bt_fn_hurt_player(control_index, 4, 0x80132ED0)
+
 typedef u32 (*bt_fnt_get_health)(u32);
 #define bt_fn_get_health ((bt_fnt_get_health)0x800A1718)
+
 typedef u32 (*bt_fnt_increase_health)(u32, s32);
 #define bt_fn_increase_health ((bt_fnt_increase_health)0x800A17A8)
+
+typedef u32 (*bt_fnt_refill_air)(u16, u16);
+#define bt_fn_refill_air ((bt_fnt_refill_air)0x800CE9E8)
 
 #endif // BT_PLAYER_CHAR_H
