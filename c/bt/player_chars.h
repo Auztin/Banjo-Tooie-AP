22cf66c0d593d0d9384473fe65fad38b9ec1836f
Austin <6369485+Auztin@users.noreply.github.com>
2024-11-22 11:01:10 -0600

    on the fly character swap

--- c/bt/player_chars.h
+++ c/bt/player_chars.h
@@ -13,8 +13,6 @@ typedef struct {
 #define bt_player_chars (*(bt_player_chars_t*)0x80135490)
 #define bt_current_player_char (bt_player_chars.loaded[bt_player_chars.control_index])
 
-#define BT_PLAYER_CHAR_CLOCKWORK 0x11
-
 typedef u32 (*bt_fnt_hurt_player)(u32, u32, u32);
 #define _bt_fn_hurt_player ((bt_fnt_hurt_player)0x800F79DC)
 #define bt_fn_hurt_player(control_index) _bt_fn_hurt_player(control_index, 4, 0x80132ED0)
@@ -27,5 +25,42 @@ typedef u32 (*bt_fnt_increase_health)(u32, s32);
 
 typedef u32 (*bt_fnt_refill_air)(u16, u16);
 #define bt_fn_refill_air ((bt_fnt_refill_air)0x800CE9E8)
+
+typedef u32 (*bt_fnt_set_character)(u32, u16);
+#define bt_fn_set_character ((bt_fnt_set_character)0x800A3410)
+
+typedef u32 (*bt_fnt_load_character_model)(u32);
+#define bt_fn_load_character_model ((bt_fnt_load_character_model)0x80092778)
+
+typedef u32 (*bt_fnt_set_character_animation)(u32, u16);
+#define bt_fn_set_character_animation ((bt_fnt_set_character_animation)0x8009E5C8)
+
+typedef u32 (*bt_fnt_get_default_animation)(u32);
+#define bt_fn_get_default_animation ((bt_fnt_get_default_animation)0x80084750)
+
+#define bt_fn_change_character(character, type) ({                                  \
+  bt_fn_set_character(character, type);                                             \
+  bt_fn_load_character_model(character);                                            \
+  bt_fn_set_character_animation(character, bt_fn_get_default_animation(character)); \
+})
+
+enum {
+  BT_PLAYER_CHAR_BANJO_KAZOOIE = 1,
+  BT_PLAYER_CHAR_SNOWBALL,
+  BT_PLAYER_CHAR_BEE = 6,
+  BT_PLAYER_CHAR_WASHER,
+  BT_PLAYER_CHAR_STONY,
+  BT_PLAYER_CHAR_BREEGULL_BLASTER,
+  BT_PLAYER_CHAR_BANJO,
+  BT_PLAYER_CHAR_KAZOOIE,
+  BT_PLAYER_CHAR_SUB,
+  BT_PLAYER_CHAR_MUMBO,
+  BT_PLAYER_CHAR_GOLDEN_GOLIATH,
+  BT_PLAYER_CHAR_DETONATOR,
+  BT_PLAYER_CHAR_VAN,
+  BT_PLAYER_CHAR_CLOCKWORK,
+  BT_PLAYER_CHAR_TREX,
+  BT_PLAYER_CHAR_DADDY_TREX,
+};
 
 #endif // BT_PLAYER_CHAR_H
