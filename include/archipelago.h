2fdaa1aa7603b4276d649aee33cb4bf420a66dbc
Austin <6369485+Auztin@users.noreply.github.com>
2024-11-28 23:16:37 -0600

    match version

--- include/archipelago.h
+++ include/archipelago.h
@@ -2,8 +2,8 @@
 #define ARCHIPELAGO_H
 
 #define AP_VERSION_MAJOR 3
-#define AP_VERSION_MINOR 4
-#define AP_VERSION_PATCH 1
+#define AP_VERSION_MINOR 5
+#define AP_VERSION_PATCH 0
 
 #include <stdint.h>
 #include <stdbool.h>
