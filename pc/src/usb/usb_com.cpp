33be253bb28ed5872376a40bd429638b2a882c29
Austin <6369485+Auztin@users.noreply.github.com>
2024-12-13 19:56:00 -0600

    reset all deathlink vars on usb reconnect

--- pc/src/usb/usb_com.cpp
+++ pc/src/usb/usb_com.cpp
@@ -153,9 +153,11 @@ void USBCom::process() {
           write(USB_CMD_PONG, packet.size);
           status = USB_STATUS_CONNECTED;
           ap_memory.n64.misc.show_message = 0;
-          ap_memory.pc.misc.show_message = 0;
+          ap_memory.n64.misc.death_link_us = 0;
+          ap_memory.n64.misc.death_link_ap = 0;
           ap_memory.pc.misc.death_link_us = 0;
           ap_memory.pc.misc.death_link_ap = 0;
+          ap_memory.pc.misc.show_message = 0;
           for (int i = 0; i < sizeof(ap_memory_pc_t); i++) ((u8*)&apm_clone)[i] = 0;
           break;
         default:
