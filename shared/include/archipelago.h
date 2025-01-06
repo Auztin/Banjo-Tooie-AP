6daf95ae27d220cf5b0db05d5e11c96ee4472567
Austin <6369485+Auztin@users.noreply.github.com>
2025-01-05 19:58:56 -0600

    version change

--- shared/include/archipelago.h
+++ shared/include/archipelago.h
@@ -2,8 +2,8 @@
 #define ARCHIPELAGO_H
 
 #define AP_VERSION_MAJOR 4
-#define AP_VERSION_MINOR 0
-#define AP_VERSION_PATCH 1
+#define AP_VERSION_MINOR 1
+#define AP_VERSION_PATCH 0
 
 #include <stdint.h>
 #include <stdbool.h>
