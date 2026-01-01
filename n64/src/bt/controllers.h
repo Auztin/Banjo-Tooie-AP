4ceb54b842b40820140b895a7c262ff7b73a201a
Austin <6369485+Auztin@users.noreply.github.com>
2025-12-31 21:51:57 -0600

    add controllers

---
+++ n64/src/bt/controllers.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <n64/types.h>
+
+typedef struct {
+	u8 a : 1;
+	u8 b : 1;
+	u8 z : 1;
+	u8 start : 1;
+	u8 dup : 1;
+	u8 ddown : 1;
+	u8 dleft : 1;
+	u8 dright : 1;
+
+	u8 _unused : 2;
+	u8 l : 1;
+	u8 r : 1;
+	u8 cup : 1;
+	u8 cdown : 1;
+	u8 cleft : 1;
+	u8 cright : 1;
+} bt_controller_buttons_t;
+
+typedef struct {
+	bt_controller_buttons_t held;
+	bt_controller_buttons_t held_delayed;
+	bt_controller_buttons_t pressed;
+	bt_controller_buttons_t released;
+	struct {
+		float x;
+		float y;
+	} joystick;
+} bt_controller_t;
+#define bt_controllers ((bt_controller_t *)0x80079AF8)
